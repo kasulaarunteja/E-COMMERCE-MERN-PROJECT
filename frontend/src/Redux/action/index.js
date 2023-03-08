00a2ac1f84dd388c3990183f7e7558a86f61d8c7
kasulaarunteja <kasulaarunteja@gmail.com>
2023-03-08 18:16:40 +0530

    index

--- frontend/src/Redux/action/index.js
+++ frontend/src/Redux/action/index.js
@@ -1,17 +1,17 @@
 /// for add item to cart
 
-export const addCart = (product) =>{
-    return {
-        type: "ADDITEM",
-        payload: product
-    }
+export const addCart = (product) => {
+  return {
+    type: 'ADDITEM',
+    payload: product,
+  }
 }
 
 // for Delete item  from cart
 
-export const delCart = (product) =>{
-    return {
-        type: "DELITEM",
-        payload: product
-    }
+export const delCart = (product) => {
+  return {
+    type: 'DELITEM',
+    payload: product,
+  }
 }
